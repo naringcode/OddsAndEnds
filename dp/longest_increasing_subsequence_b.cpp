c9ebca9a41397105a094f27d3a055e8146234b4c
naringcode <naringcode@gmail.com>
2023-07-26 19:53:16 +0900

    modified

--- dp/longest_increasing_subsequence_b.cpp
+++ dp/longest_increasing_subsequence_b.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 
+using namespace std;
+
 // Time Complexity : O(NlogN)
 //
 // 최장 길이만 구함
@@ -13,8 +15,47 @@
 // 4 : 10 15 20 50 0 0 0
 // 4 : 10 12 20 50 0 0 0 <- 15가 12로 바뀜
 
+int n;
+
+int arr[1'000'004];
+int lis[1'000'004];
+
+int len;
+
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        int* lower = lower_bound(lis, lis + len, arr[i]);
+
+        // 새로운 최장 길이를 찾으면
+        if (0 == *lower)
+        {
+            len++;
+        }
+
+        *lower = arr[i];
+
+        cout << len << " : ";
+
+        for (int j = 0; j < n; j++)
+        {
+            cout << lis[j] << ' ';
+        }
+
+        cout << '\n';
+    }
 
     return 0;
 }
